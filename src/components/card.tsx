7cd54768ef977ab77e73b398c18a542fbf098438
SFToro <sftpo@protonmail.ch>
2023-06-28 17:13:55 +0200

    chore: Wrapped ion card on reused card component

---
+++ src/components/card.tsx
@@ -0,0 +1,28 @@
+import type { Movie } from "../utils/movies";
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+} from "@ionic/react";
+import "./card.css";
+
+export default function MediaCard({ movie }: { movie: Movie }) {
+  return (
+    <IonCard>
+      <img
+        alt={`Póster de la película ${movie.title}`}
+        src={`https://image.tmdb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
+      />
+      <IonCardHeader>
+        <IonCardTitle>{movie.title}</IonCardTitle>
+        <IonCardSubtitle>{`${movie.vote_average}`}</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>
+        <p>{movie.overview}</p>
+      </IonCardContent>
+    </IonCard>
+  );
+}
