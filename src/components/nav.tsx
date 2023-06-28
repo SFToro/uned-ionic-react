952aae14d99b9cf83db296e15aa5e69b7f97d320
SFToro <sftpo@protonmail.ch>
2023-06-28 17:14:13 +0200

    chore: Wrapped IonHeader on reused nav component

---
+++ src/components/nav.tsx
@@ -0,0 +1,13 @@
+import { IonFooter, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+
+export default function ButtonAppBar() {
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>UNED Pelis</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </>
+  );
+}
